49836e1a9f10a87a8527b97e65403f5c4e3862d1
jasonou <jasonou95@gmail.com>
2016-11-23 20:50:05 -0800

    Basic Test Set Up

---
+++ next-beer-android/app/src/androidTest/java/com/herokuapp/nextbeerback/nextbeer/HistoryTest.java
@@ -0,0 +1,22 @@
+package com.herokuapp.nextbeerback.nextbeer;
+
+import android.app.Instrumentation;
+import android.test.InstrumentationTestCase;
+
+/**
+ * Created by innouvation on 11/23/16.
+ */
+
+public class HistoryTest extends InstrumentationTestCase {
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+}
---
+++ next-beer-android/app/src/androidTest/java/com/herokuapp/nextbeerback/nextbeer/ProfileTest.java
@@ -0,0 +1,21 @@
+package com.herokuapp.nextbeerback.nextbeer;
+
+import android.test.InstrumentationTestCase;
+
+/**
+ * Created by innouvation on 11/23/16.
+ */
+
+public class ProfileTest extends InstrumentationTestCase {
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+}
